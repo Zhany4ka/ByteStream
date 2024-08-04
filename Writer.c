4d3bb107204844e1c7b0d86eeb575e5a86e8d666
Zhany4ka <87239420+Zhany4ka@users.noreply.github.com>
2024-08-04 21:34:30 +0400

    Update Writer.c

--- Writer.c
+++ Writer.c
@@ -3,80 +3,72 @@
 #include <vector>
 #include <zlib.h>
 #include <bitset>
+#include <stdexcept>
 
 using namespace std;
 
 class Writer {
- public:
-  static void WriteScString(vector<unsigned char>& buffer, const string& value) {
-    if (value.empty()) {
-      buffer.push_back(0);
-      buffer.push_back(0);
-      buffer.push_back(0);
-      buffer.push_back(0);
-    } else {
-      int length = value.size();
-      buffer.push_back((length >> 24) & 0xff);
-      buffer.push_back((length >> 16) & 0xff);
-      buffer.push_back((length >> 8) & 0xff);
-      buffer.push_back(length & 0xff);
-
-      for (int i = 0; i < length; i++) {
-        buffer.push_back(value[i]);
-      }
-    }
-  }
-
-  static void WriteVInt(vector<unsigned char>& buffer, int value) {
-    bitset<32> bits(value);
-    int length = bits.count();
-
-    int first = (1 << (7 - length % 7)) - 1;
-    first |= (value & ((1 << (7 - length % 7)) - 1)) << (length % 7);
-    buffer.push_back(first);
-
-    int pos = 7 - length % 7;
-    while (pos < 32) {
-      int next = value >> pos & 0x7f;
-      if (pos + 7 < 32) {
-        next |= 0x80;
-      }
-      buffer.push_back(next);
-      pos += 7;
-    }
-  }
-
-  static void WriteScId(vector<unsigned char>& buffer, int high, int low) {
-    WriteVInt(buffer, high);
-    WriteVInt(buffer, low);
-  }
-
-  static void WriteCompressedString(vector<unsigned char>& buffer,
-                                     const string& value, bool indicate) {
-    int dataLength = value.size();
-    vector<unsigned char> data(dataLength);
-    for (int i = 0; i < dataLength; i++) {
-      data[i] = value[i];
+public:
+    static void WriteScString(vector<unsigned char>& buffer, const string& value) {
+        if (value.empty()) {
+            buffer.insert(buffer.end(), 4, 0);
+        } else {
+            int length = value.size();
+            WriteIntToBuffer(buffer, length);
+            buffer.insert(buffer.end(), value.begin(), value.end());
+        }
     }
 
-    uLongf compressedLength = compressBound(dataLength);
-    vector<unsigned char> compressed(compressedLength);
+    static void WriteVInt(vector<unsigned char>& buffer, int value) {
+        if (value < 0) {
+            throw invalid_argument("Negative values are not supported.");
+        }
 
-    compress(&compressed[0], &compressedLength, &data[0], dataLength);
-
-    if (indicate) {
-      buffer.push_back(1);
+        if (value <= 0x3F) {
+            buffer.push_back(static_cast<unsigned char>(value & 0x3F));
+        } else if (value < 0x2000) {
+            buffer.push_back(static_cast<unsigned char>((value & 0x3F) | 0x80));
+            buffer.push_back(static_cast<unsigned char>((value >> 6) & 0x7F));
+        } else if (value < 0x100000) {
+            buffer.push_back(static_cast<unsigned char>((value & 0x3F) | 0x80));
+            buffer.push_back(static_cast<unsigned char>(((value >> 6) & 0x3F) | 0x80));
+            buffer.push_back(static_cast<unsigned char>((value >> 13) & 0x7F));
+        } else {
+            buffer.push_back(static_cast<unsigned char>((value & 0x3F) | 0x80));
+            buffer.push_back(static_cast<unsigned char>(((value >> 6) & 0x3F) | 0x80));
+            buffer.push_back(static_cast<unsigned char>(((value >> 13) & 0x3F) | 0x80));
+            buffer.push_back(static_cast<unsigned char>((value >> 20) & 0x7F));
+        }
     }
 
-    buffer.push_back((compressedLength + 4) >> 24 & 0xff);
-    buffer.push_back((compressedLength + 4) >> 16 & 0xff);
-    buffer.push_back((compressedLength + 4) >> 8 & 0xff);
-    buffer.push_back((compressedLength + 4) & 0xff);
+    static void WriteScId(vector<unsigned char>& buffer, int high, int low) {
+        WriteVInt(buffer, high);
+        WriteVInt(buffer, low);
+    }
 
-    buffer.push_back(dataLength >> 24 & 0xff);
-    buffer.push_back(dataLength >> 16 & 0xff);
-    buffer.push_back(dataLength >> 8 & 0xff);
-    buffer.push_back(dataLength & 0xff);
+    static void WriteCompressedString(vector<unsigned char>& buffer, const string& value, bool indicate) {
+        vector<unsigned char> data(value.begin(), value.end());
+        uLongf compressedLength = compressBound(data.size());
+        vector<unsigned char> compressed(compressedLength);
 
-  }
-     
+        if (compress(compressed.data(), &compressedLength, data.data(), data.size()) != Z_OK) {
+            throw runtime_error("Compression failed.");
+        }
+
+        if (indicate) {
+            buffer.push_back(1);
+        }
+
+        WriteIntToBuffer(buffer, compressedLength + 4); 
+        WriteIntToBuffer(buffer, data.size()); 
+        buffer.insert(buffer.end(), compressed.begin(), compressed.begin() + compressedLength);
+    }
+
+private:
+    static void WriteIntToBuffer(vector<unsigned char>& buffer, int value) {
+        buffer.push_back((value >> 24) & 0xff);
+        buffer.push_back((value >> 16) & 0xff);
+        buffer.push_back((value >> 8) & 0xff);
+        buffer.push_back(value & 0xff);
+    }
+};
